27ec410830cbfcbe418ad1fbcd7e90136675a8d1
sunyoon-Hwang <83747377+sunyoon-Hwang@users.noreply.github.com>
2021-05-22 16:45:05 +0900

    Update shell.c

--- shell.c
+++ shell.c
@@ -32,10 +32,10 @@ int main(void) {
         int len;
         bool cd = false;
 
-	    char strBuffer[_MAX_PATH] = { 0, };
-	    char *pstrBuffer = NULL;
-	    pstrBuffer = getcwd( strBuffer, _MAX_PATH );
-	    char bin[30] = "/bin/";
+	char strBuffer[_MAX_PATH] = { 0, };
+	char *pstrBuffer = NULL;
+	pstrBuffer = getcwd( strBuffer, _MAX_PATH );
+	char bin[30] = "/bin/";
 
         printf("\033[92m%s@%s\033[0m:\033[94m%s\033[0m$ ",getpwuid(getuid())->pw_name,hostname,pstrBuffer);
         s = fgets(command, MAX_LEN_LINE, stdin);
@@ -52,29 +52,29 @@ int main(void) {
 
     	if(!strcmp(command,"exit")){
 	    	printf("program exit!\n");
-		    exit(0);
+		exit(0);
     	}
 
-	    arg1 = strtok(command," ");
+	arg1 = strtok(command," ");
 
     	if(!strcmp(arg1,"cd")){
 	    	cd = true;
-		    arg1 = strtok(NULL, " ");
-		    if(chdir(arg1)<0){
-			    printf("ERROR : Inexistence Directory [%s].\n",arg1);
-			    continue;
-		    }
-		    else{
-			    printf("change directory successfully!\n");
-			    continue;
-		    }
-	    }
+		arg1 = strtok(NULL, " ");
+		if(chdir(arg1)<0){
+		    printf("ERROR : Inexistence Directory [%s].\n",arg1);
+		    continue;
+		}
+		else{
+		    printf("change directory successfully!\n");
+		    continue;
+		}
+	 }
 
         printf("[%s]\n\n", command);
 
-	    if(!cd) {
-		    pid = fork();
-	    	if (pid < 0) {
+	if(!cd) {
+	    pid = fork();
+	    if (pid < 0) {
        	        fprintf(stderr, "fork failed\n");
          	    exit(1);
             }
@@ -93,8 +93,8 @@ int main(void) {
         		arg1 = bin;
 	        	args[0] = arg1;
    		    	args[1] = strtok(NULL," ");
-    		    args[2] = strtok(NULL," ");
-    	    	args[3] = strtok(NULL," ");
+    		        args[2] = strtok(NULL," ");
+    	    		args[3] = strtok(NULL," ");
 	    
 		        ret = execve(args[0], args, NULL);
    			    if (ret < 0) {
